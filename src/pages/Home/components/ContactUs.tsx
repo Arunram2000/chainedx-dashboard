29134bd50a3d6da2cffc6e040c0e61615b29e470
Arunachalam N <arunachalam@Arunachalams-MacBook-Pro.local>
2022-04-20 21:19:02 +0530

    choke:responsiveness on web

--- src/pages/Home/components/ContactUs.tsx
+++ src/pages/Home/components/ContactUs.tsx
@@ -33,7 +33,7 @@ const ContactUs: React.FC = () => {
     <div className="contactus">
       <div className="max pad">
         <div className="contactus_container">
-          <div className="contact_info" data-aos="flip-right">
+          <div className="contact_info" data-aos="flip-up">
             <h1 className="mb-50">
               Contact with <span className="color-primary">us</span>
             </h1>
@@ -93,7 +93,7 @@ const ContactUs: React.FC = () => {
               </div>
             </div>
           </div>
-          <div className="contact_form" data-aos="flip-left">
+          <div className="contact_form" data-aos="flip-down">
             <h1 className="mb-50">
               Leave a Messag<span className="color-primary">e</span>
             </h1>
